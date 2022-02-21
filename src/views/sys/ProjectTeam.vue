f52ec94421ff532323a7f51d61f0e1618382cec1
wozhendeshuai <jiafeng1997@gmail.com>
2022-02-21 21:44:46 +0800

    开始团队页面

--- src/views/sys/ProjectTeam.vue
+++ src/views/sys/ProjectTeam.vue
@@ -1,185 +1,19 @@
 <template>
   <div>
-    <el-form :inline="true">
-      <el-form-item>
-        <el-input
-            v-model="searchForm.username"
-            placeholder="用户名"
-            clearable
-        >
-        </el-input>
-      </el-form-item>
-
-      <el-form-item>
-        <el-button @click="getUserList">搜索</el-button>
-      </el-form-item>
-
-      <el-form-item>
-        <el-button type="primary" @click="dialogVisible = true" v-if="hasAuth('sys:user:save')">新增</el-button>
-      </el-form-item>
-      <el-form-item>
-        <el-popconfirm title="这是确定批量删除吗？" @confirm="delHandle(null)">
-          <el-button type="danger" slot="reference" :disabled="delBtlStatu" v-if="hasAuth('sys:user:delete')">批量删除
-          </el-button>
-        </el-popconfirm>
-      </el-form-item>
-    </el-form>
-
-    <el-table
-        ref="multipleTable"
-        :data="tableData"
-        tooltip-effect="dark"
-        style="width: 100%"
-        border
-        stripe
-        @selection-change="handleSelectionChange">
-
-      <el-table-column
-          type="selection"
-          width="55">
-      </el-table-column>
-
-      <el-table-column
-          label="头像"
-          width="50">
-        <template slot-scope="scope">
-          <el-avatar size="small" :src="scope.row.avatar"></el-avatar>
-        </template>
-      </el-table-column>
-
-      <el-table-column
-          prop="username"
-          label="用户名"
-          width="120">
-      </el-table-column>
-      <el-table-column
-          prop="code"
-          label="角色名称">
-        <template slot-scope="scope">
-          <el-tag size="small" type="info" v-for="item in scope.row.sysRoles">{{ item.name }}</el-tag>
-        </template>
-
-      </el-table-column>
-      <el-table-column
-          prop="email"
-          label="邮箱">
-      </el-table-column>
-      <el-table-column
-          prop="phone"
-          label="手机号">
-      </el-table-column>
-
-      <el-table-column
-          prop="statu"
-          label="状态">
-        <template slot-scope="scope">
-          <el-tag size="small" v-if="scope.row.statu === 1" type="success">正常</el-tag>
-          <el-tag size="small" v-else-if="scope.row.statu === 0" type="danger">禁用</el-tag>
-        </template>
-
-      </el-table-column>
-      <el-table-column
-          prop="created"
-          width="200"
-          label="创建时间"
-      >
-      </el-table-column>
-      <el-table-column
-          prop="icon"
-          width="260px"
-          label="操作">
-
-        <template slot-scope="scope">
-          <el-button type="text" @click="roleHandle(scope.row.id)">分配角色</el-button>
-          <el-divider direction="vertical"></el-divider>
-
-          <el-button type="text" @click="repassHandle(scope.row.id, scope.row.username)">重置密码</el-button>
-          <el-divider direction="vertical"></el-divider>
-
-          <el-button type="text" @click="editHandle(scope.row.id)">编辑</el-button>
-          <el-divider direction="vertical"></el-divider>
-
-          <template>
-            <el-popconfirm title="这是一段内容确定删除吗？" @confirm="delHandle(scope.row.id)">
-              <el-button type="text" slot="reference">删除</el-button>
-            </el-popconfirm>
-          </template>
-
-        </template>
-      </el-table-column>
-
-    </el-table>
-
-    <el-pagination
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        layout="total, sizes, prev, pager, next, jumper"
-        :page-sizes="[10, 20, 50, 100]"
-        :current-page="current"
-        :page-size="size"
-        :total="total">
-    </el-pagination>
-
-
-    <!--新增对话框-->
-    <el-dialog
-        title="提示"
-        :visible.sync="dialogVisible"
-        width="600px"
-        :before-close="handleClose">
-
-      <el-form :model="editForm" :rules="editFormRules" ref="editForm">
-        <el-form-item label="用户名" prop="username" label-width="100px">
-          <el-input v-model="editForm.username" autocomplete="off"></el-input>
-          <el-alert
-              title="初始密码为888888"
-              :closable="false"
-              type="info"
-              style="line-height: 12px;"
-          ></el-alert>
-        </el-form-item>
-
-        <el-form-item label="邮箱" prop="email" label-width="100px">
-          <el-input v-model="editForm.email" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="手机号" prop="phone" label-width="100px">
-          <el-input v-model="editForm.phone" autocomplete="off"></el-input>
-        </el-form-item>
-
-        <el-form-item label="状态" prop="statu" label-width="100px">
-          <el-radio-group v-model="editForm.statu">
-            <el-radio :label="0">禁用</el-radio>
-            <el-radio :label="1">正常</el-radio>
-          </el-radio-group>
-        </el-form-item>
-
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="resetForm('editForm')">取 消</el-button>
-        <el-button type="primary" @click="submitForm('editForm')">确 定</el-button>
-      </div>
-    </el-dialog>
-
-    <!-- 分配权限对话框 -->
-    <el-dialog title="分配角色" :visible.sync="roleDialogFormVisible" width="600px">
-
-      <el-form :model="roleForm">
-        <el-tree
-            :data="roleTreeData"
-            show-checkbox
-            ref="roleTree"
-            :check-strictly=checkStrictly
-            node-key="id"
-            :default-expand-all=true
-            :props="defaultProps">
-        </el-tree>
-      </el-form>
-
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="roleDialogFormVisible=false">取 消</el-button>
-        <el-button type="primary" @click="submitRoleHandle('roleForm')">确 定</el-button>
-      </div>
-    </el-dialog>
+    <el-row>
+      <el-col :span="8" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 2 : 0">
+        <el-card :body-style="{ padding: '0px' }">
+          <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
+          <div style="padding: 14px;">
+            <span>好吃的汉堡</span>
+            <div class="bottom clearfix">
+              <time class="time">{{ currentDate }}</time>
+              <el-button type="text" class="button">操作按钮</el-button>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
 
   </div>
 </template>
