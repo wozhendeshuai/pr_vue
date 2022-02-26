06a29af43a0d8f6099d6f2ca28e07f2548b540e5
wozhendeshuai <jiafeng1997@gmail.com>
2022-02-26 20:01:44 +0800

    开始团队页面

--- src/views/pr/PRList.vue
+++ src/views/pr/PRList.vue
@@ -107,13 +107,18 @@
           </el-button>
           <el-button
               size="mini"
-              type="danger"
+              type="primary"
               @click="handleMerge(scope.$index, scope.row)">合入PR
           </el-button>
           <el-button
               size="mini"
               type="warning"
               @click="reviewPR(scope.$index, scope.row)">评审PR
+          </el-button>
+          <el-button
+              size="mini"
+              type="danger"
+              @click="closePR(scope.$index, scope.row)">关闭PR
           </el-button>
         </template>
       </el-table-column>
@@ -136,10 +141,52 @@
             <el-option v-for="temp in branchs" :value="temp.name">{{ temp.name }}</el-option>
           </el-select>
         </el-form-item>
+        <el-form-item label="PR标题" prop="name">
+          <el-input v-model="newPRTitle"></el-input>
+        </el-form-item>
+        <el-form-item label="PR内容" prop="desc">
+          <el-input type="textarea" v-model="newPRBody"></el-input>
+        </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
     <el-button type="danger" @click="newPRVisible = false">取 消</el-button>
     <el-button type="primary" @click="toNewPR()">确 定</el-button>
+     </span>
+    </el-dialog>
+    <el-dialog
+        :visible.sync="isMerge"
+        width="60%"
+        center>
+      <span slot="title" class="dialog-title" center>
+        项目{{ choiceRepoName }} 合并{{ choicePRDetail.prNumber }}号PR
+      </span>
+      <el-form>
+        <el-form-item label="PR名称:">
+          {{ choicePRDetail.title }}
+        </el-form-item>
+        <el-form-item label="merge名称：" prop="name">
+          <el-input v-model="mergeTitle"></el-input>
+        </el-form-item>
+        <el-form-item label="merge消息：" prop="desc">
+          <el-input type="textarea" v-model="mergeMessage"></el-input>
+        </el-form-item>
+        <el-form-item label="merge方式:">
+          <el-radio-group v-model="mergeMethod">
+            <el-radio label='merge'>合入发生改变的分支中每次提交</el-radio>
+            <el-radio label="squash">将发生改变的分支中多个commit合并为一个commit</el-radio>
+            <el-radio label="rebase">将发生改变的分支中的每一次提交都rebase到原仓库</el-radio>
+          </el-radio-group>
+<!--          <el-radio-group v-model="mergeMethod">-->
+<!--            <el-radio-button label="merge">合入发生改变的分支中每次提交</el-radio-button>-->
+<!--            <el-radio-button label="squash">将发生改变的分支中多个commit合并为一个commit</el-radio-button>-->
+<!--            <el-radio-button label="rebase">将发生改变的分支中的每一次提交都rebase到原仓库</el-radio-button>-->
+<!--          </el-radio-group>-->
+        </el-form-item>
+
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+    <el-button type="danger" @click="isMerge = false">取 消</el-button>
+    <el-button type="primary" @click="toMergePR()">确 定</el-button>
      </span>
     </el-dialog>
   </div>
@@ -150,7 +197,14 @@ export default {
   name: "Role",
   data() {
     return {
+      isMerge: false,
+      choicePRDetail: {},
+      mergeTitle: '',
+      mergeMessage: '',
+      mergeMethod: 'merge',
 
+      newPRTitle: '',
+      newPRBody: '',
       newPRVisible: false,
       repoList: [],
       isChoiceModel: true,
@@ -171,6 +225,7 @@ export default {
       branchsNameList: [],
       baseBranch: '',
       compareBranch: '',
+      choiceFullName: "1",
 
     }
   },
@@ -185,14 +240,102 @@ export default {
     })
   },
   methods: {
+    toMergePR() {
+      console.log(this.choiceRepoName)
+      console.log(this.mergeTitle)
+      console.log(this.mergeMessage)
+      console.log(this.mergeMethod)
+
+      for (let i = 0; i < this.repoList.length; i++) {
+        if (this.repoList[i].repoName === this.choiceRepoName) {
+          this.choiceFullName = this.repoList[i].teamName
+          break
+        }
+      }
+      console.log(this.choiceFullName)
+      var teamNameArr = this.choiceFullName.split("/")
+      var teamName = teamNameArr[0]
+      console.log(teamName)
+
+      this.$axios.get('/prManage/prBase/mergePR?userName=' + this.userName
+          + '&prNumber=' + this.choicePRDetail.prNumber
+          + '&ownerName=' + teamName
+          + '&repoName=' + this.choiceRepoName
+          + '&commitTitle=' + this.mergeTitle
+          + '&commitMessage=' + this.mergeMessage
+          + '&mergeMethod=' + this.mergeMethod
+         ).then(res => {
+        console.log(res.data.data)
+        alert(res.data.data)
+      })
+      this.isMerge = false
+    },
+
+    handleMerge(index, row) {
+      console.log("选中的在table中的index是：" + index);
+      console.log("选中的在table中的prnumber是：" + row.prNumber);
+      this.choicePRDetail = row
+      console.log(this.choicePRDetail)
+      this.isMerge = true
+    },
     toNewPR() {
       console.log(this.baseBranch)
       console.log(this.compareBranch)
+      console.log(this.newPRTitle)
+      console.log(this.newPRBody)
+      console.log(this.choiceRepoName)
+
+      for (let i = 0; i < this.repoList.length; i++) {
+        if (this.repoList[i].repoName === this.choiceRepoName) {
+          this.choiceFullName = this.repoList[i].teamName
+          break
+        }
+      }
+      console.log(this.choiceFullName)
+      var teamNameArr = this.choiceFullName.split("/")
+      var teamName = teamNameArr[0]
+      console.log(teamName)
       if (this.baseBranch === this.compareBranch) {
         alert("不可以选择同一分支")
       } else {
+        this.$axios.get('/prManage/prBase/newPR?userName=' + this.userName
+            + '&repoName=' + this.choiceRepoName
+            + '&ownerName=' + teamName
+            + '&baseBranch=' + this.baseBranch
+            + '&compareBranch=' + this.compareBranch
+            + '&prTitle=' + this.newPRTitle
+            + '&prContent=' + this.newPRBody).then(res => {
+          console.log(res.data.data)
+          alert(res.data.data)
+        })
         this.newPRVisible = false
       }
+    },
+    closePR(index, row) {
+      console.log("选中的在table中的index是：" + index);
+      console.log("选中的在table中的prnumber是：" + row.prNumber);
+      console.log(this.choiceRepoName)
+
+      for (let i = 0; i < this.repoList.length; i++) {
+        if (this.repoList[i].repoName === this.choiceRepoName) {
+          this.choiceFullName = this.repoList[i].teamName
+          break
+        }
+      }
+      console.log(this.choiceFullName)
+      var teamNameArr = this.choiceFullName.split("/")
+      var teamName = teamNameArr[0]
+      console.log(teamName)
+
+      this.$axios.get('/prManage/prBase/closePR?userName=' + this.userName
+          + '&repoName=' + this.choiceRepoName
+          + '&ownerName=' + teamName
+          + '&prNumber=' + row.prNumber).then(res => {
+        console.log(res.data.data)
+        alert(res.data.data)
+      })
+      this.newPRVisible = false
+
     },
     newPR() {
       this.newPRVisible = true
@@ -234,10 +377,7 @@ export default {
       console.log("选中的在table中的prnumber是：" + row.prNumber);
       this.$router.push({path: "/pr/detail", query: {repoName: this.choiceRepoName, prNumber: row.prNumber}})
     },
-    handleMerge(index, row) {
-      console.log("选中的在table中的index是：" + index);
-      console.log("选中的在table中的prnumber是：" + row.prNumber);
-    },
+
     getModelDetail() {
       console.log("选中的排序规则是：" + this.choiceModel);
       console.log("仓库是" + this.choiceRepoName)
